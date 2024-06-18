09a7e2f8726387535d9ce03f69e880ac6dca2caa
Saatarko <saatarko@tut.by>
2024-06-18 17:25:34 +0300

    DRF start2

--- DRF/main/admin.py
+++ DRF/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Bank, Currency, Exchange
+
+
 # Register your models here.
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'UNN')
+    list_display_links = ('id', 'name', 'UNN')
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'currency_name', 'slug')
+    list_display_links = ('id', 'currency_name', 'slug')
+
+@admin.register(Exchange)
+class ExchangeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'currency_exchange', 'data', 'bank', 'currency')
+    list_display_links = ('id', 'currency_exchange', 'data', 'bank', 'currency')
--- DRF/main/models.py
+++ DRF/main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Bank(models.Model):
+    name = models.CharField('Название банка', max_length=100)
+    UNN = models.IntegerField('УНН банка')
+
+    def __str__(self):
+        return f'{self.name}{self.UNN}'
+
+    class Meta:
+        verbose_name = 'Банки'
+        verbose_name_plural = 'Банки'
+
+
+class Currency(models.Model):
+    currency_name = models.CharField('Название валюты', max_length=20)
+    slug = models.SlugField('Слаг валюты', max_length=20)
+
+    def __str__(self):
+        return self.currency_name
+    class Meta:
+        verbose_name = 'Валюты'
+        verbose_name_plural = 'Валюты'
+
+class Exchange(models.Model):
+    currency_exchange = models.IntegerField('курс валюты')
+    data = models.DateField('дата курса')
+
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.currency} {self.data}"
+
+    class Meta:
+        verbose_name = 'Курсы валют'
+        verbose_name_plural = 'Курсы валют'
